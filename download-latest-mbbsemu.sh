b374d8d125a70d634088947cec68043ce42f7973
Matt Fletcher <fletcherm@gmail.com>
2021-07-07 14:25:38 -0400

    Unzip the mbbsemu package

--- download-latest-mbbsemu.sh
+++ download-latest-mbbsemu.sh
@@ -2,8 +2,14 @@
 
 mkdir -p pkg/archive/mbbsemu
 mv pkg/mbbsemu-* pkg/archive/mbbsemu
+
 pushd .
 cd pkg
+
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" $1
+basename=`ls *.zip | sed s/\.zip$//`
+unzip -d $basename *.zip
+rm mbbsemu*.zip
+
 popd
 
