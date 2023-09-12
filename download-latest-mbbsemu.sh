de47a9109937e5cc357a4ea6cc1763ba5d498a30
Matt Fletcher <fletcherm@gmail.com>
2023-09-11 20:17:58 -0400

    Update download script to pull from Github Actions instead of mbbsemu website.

--- download-latest-mbbsemu.sh
+++ download-latest-mbbsemu.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+set -e
 set -x
 
-if [ $# -eq 0 ]; then
- echo 'Must pass in the name of the download URL. Download URLs are listed at `https://www.mbbsemu.com/Downloads/master`.'
- exit 1
+# Check for the presence of the gh command
+if ! command -v gh &> /dev/null; then
+    echo "The 'gh' command is not found. Please install it from https://github.com/cli/cli/blob/trunk/docs/install_linux.md"
+    exit 1
 fi
 
-mkdir -p pkg/archive/mbbsemu
-mv pkg/mbbsemu-* pkg/archive/mbbsemu
+REPO="mbbsemu/mbbsemu"
+WORKFLOW="dotnet.yml"
+PATTERN="mbbsemu-linux-x64-*"
+PKG_DIR="pkg"
+ARCHIVE_DIR="$PKG_DIR/archive/mbbsemu"
 
-pushd .
-cd pkg
+echo "Fetching the latest successful run ID for $REPO on $WORKFLOW."
+RUN_ID=$(gh run list --repo "$REPO" --workflow "$WORKFLOW" --status success --branch master --json databaseId --jq '.[0].databaseId')
 
-wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" $1
-basename=`ls *.zip | sed s/\.zip$//`
-unzip -d $basename *.zip
-rm mbbsemu*.zip
+if [ -z "$RUN_ID" ]; then
+    echo "Error: Could not retrieve a successful run ID."
+    exit 1
+fi
 
-popd
+echo "Archiving old mbbsemu builds."
+mkdir -p "$ARCHIVE_DIR"
+mv "$PKG_DIR"/mbbsemu-*/* "$ARCHIVE_DIR"/ 2>/dev/null
 
+echo "Downloading the artifact matching pattern $PATTERN for run ID $RUN_ID."
+gh run download --repo "$REPO" "$RUN_ID" -p "$PATTERN" -D $PKG_DIR
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download the artifact."
+    exit 1
+fi
+
+echo "Download and extraction complete."
