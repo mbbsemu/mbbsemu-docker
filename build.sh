ace9c12a1cfca24e5b9efa5bd90905c3fbd956a7
Matt Fletcher <fletcherm@gmail.com>
2023-10-05 07:12:13 -0400

    Make some improvements after running through shellcheck.net linter.

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-build_date=`date "+%Y-%m-%d"`
-version=`ls -1d pkg/mbbsemu-linux-x64-* | tail -1 | sed "s/pkg\/mbbsemu-linux-x64-/ /"`
+build_date=$(date "+%Y-%m-%d")
+version=$(ls -1d pkg/mbbsemu-linux-x64-* | tail -1 | sed "s/pkg\/mbbsemu-linux-x64-//")
 
 # Strip the newline off version - https://stackoverflow.com/questions/12524308/bash-strip-trailing-linebreak-from-output
-version=$(printf $version)
+version=$(printf %s "$version")
 
 docker build \
-  --build-arg BUILD_DATE=$build_date \
-  --build-arg VERSION=$version \
+  --build-arg BUILD_DATE="$build_date" \
+  --build-arg VERSION="$version" \
   -t mbbsemu .
 
